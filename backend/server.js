12a33561d20da11bcd19af7669106e6cf7e85ec0
Sayantan Roy <sushant19101@iiitnr.edu.in>
2022-09-09 20:28:14 +0530

    dumy

--- backend/server.js
+++ backend/server.js
@@ -1,74 +1,74 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import Stripe from "stripe"
-import bodyParser from "body-parser"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import Stripe from "stripe";
+import bodyParser from "body-parser";
 const stripe = new Stripe(
-	"sk_test_51Lg3dwKkKO8NA6ZZ8PDgogdqDpBGkpmPqOCQ9TdyP0yYsIP9zgWqpeErCeUXLorHZ8xHcbYZGKOnFChpyzQlDCiT00em6mYpSN"
-)
+  "sk_test_51Lg3dwKkKO8NA6ZZ8PDgogdqDpBGkpmPqOCQ9TdyP0yYsIP9zgWqpeErCeUXLorHZ8xHcbYZGKOnFChpyzQlDCiT00em6mYpSN"
+);
 
-import { notFound, errorHandler } from "./middlewares/errorMiddleware.js"
-import connectDB from "./config/db.js"
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
-import tablesRoutes from "./routes/tablesRoutes.js"
-import bookingRoutes from "./routes/bookingRoutes.js"
-import paymentRoutes from "./routes/paymentRoutes.js"
+import tablesRoutes from "./routes/tablesRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use("/api/tables", tablesRoutes)
-app.use("/api/bookings", bookingRoutes)
-app.use("/api/payment", paymentRoutes)
+app.use("/api/tables", tablesRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/payment", paymentRoutes);
 
 // This is your Stripe CLI webhook secret for testing your endpoint locally.
 
 app.post(
-	"/webhook",
-	bodyParser.raw({ type: "application/json" }),
-	async (req, res) => {
-		// Simple deserialization:
-		// const event = JSON.parse(req.body);
-		// console.log(event.type);
-		// console.log(event.data.object);
-		// console.log(event.data.object.id);
-		// With signature verification:
-		const payload = req.body
-		const sig = req.headers["stripe-signature"]
-		const endpointSecret =
-			"whsec_efa983bb23b9f3dccb53d015aa4494a5b96391fa1066721a1ae8d73c47ee554a"
+  "/webhook",
+  bodyParser.raw({ type: "application/json" }),
+  async (req, res) => {
+    // Simple deserialization:
+    // const event = JSON.parse(req.body);
+    // console.log(event.type);
+    // console.log(event.data.object);
+    // console.log(event.data.object.id);
+    // With signature verification:
+    const payload = req.body;
+    const sig = req.headers["stripe-signature"];
+    const endpointSecret =
+      "whsec_efa983bb23b9f3dccb53d015aa4494a5b96391fa1066721a1ae8d73c47ee554a";
 
-		let event
+    let event;
 
-		try {
-			event = stripe.webhooks.constructEvent(payload, sig, endpointSecret)
-		} catch (error) {
-			console.log(error.message)
-			res.status(400).json({ success: false })
-			return
-		}
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+    } catch (error) {
+      console.log(error.message);
+      res.status(400).json({ success: false });
+      return;
+    }
 
-		console.log(event.type)
-		console.log(event.data.object)
-		console.log(event.data.object.id)
+    console.log(event.type);
+    console.log(event.data.object);
+    console.log(event.data.object.id);
 
-		res.json({ success: true })
-	}
-)
+    res.json({ success: true });
+  }
+);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 // const PORT = process.env.PORT || 5000
-const PORT = 4242
+const PORT = 5000;
 
 app.listen(
-	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
