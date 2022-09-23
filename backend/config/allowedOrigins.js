497f384b01a44ed2b008586ee9006ed80f6b7d26
Sayantan Roy <sushant19101@iiitnr.edu.in>
2022-09-23 22:54:28 +0530

    added domain name in CORS

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
-	"http://localhost:3000",
-	"http://localhost:3001",
-	"http://3.86.178.228",
-]
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://3.86.178.228",
+  "http://stiamivip.com",
+];
 
-export default allowedOrigins
+export default allowedOrigins;
